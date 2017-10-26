fa038ac4bb673433c091145d9effdccc5b8dcca1
ShadowWolf <league@WTS15.attlocal.net>
2017-10-25 18:26:34 -0700

    10/25/17
    Module 2(IP)

--- src/My_Program/UNEncrypter.java
+++ src/My_Program/UNEncrypter.java
@@ -1,7 +1,40 @@
 package My_Program;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
 public class UNEncrypter {
 public static void main(String[] args) {
-	
+String fileName = "";
+	String yunencrypter = "";
+	JFileChooser jfc = new JFileChooser();
+	int returnVal = jfc.showOpenDialog(null);
+	if (returnVal == JFileChooser.APPROVE_OPTION) {
+		 fileName = jfc.getSelectedFile().getAbsolutePath();
+		System.out.println(fileName);
+	}
+	try {
+		FileReader fr = new FileReader(fileName);
+		int c = fr.read();
+		String fileContents= "";
+		while( c != -1) {
+			fileContents += (char)((c - (fileContents.length()+10))/2);
+		
+			c = fr.read();
+		}
+		fr.close();
+		System.out.println(fileContents);
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+
 }
 }

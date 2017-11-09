512c7ec4279a843162e81bb781b75d3edc1c4065
GabrielZ1 <League@AdministorsiMac.attlocal.net>
2017-11-08 18:48:53 -0800

    11/8/17
    Level 3 Module 2 Finished

--- src/My_Program/ListTracker.java
+++ src/My_Program/ListTracker.java
@@ -2,12 +2,12 @@ package My_Program;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -17,13 +17,12 @@ public class ListTracker implements ActionListener {
 	ArrayList<String> Task = new ArrayList<String>();
 	JFrame f = new JFrame();
 	JPanel p = new JPanel();
-	JButton b = new JButton();
-	JButton b2 = new JButton();
-	JButton b3 = new JButton();
-	JButton b4 = new JButton();
+	JButton add = new JButton();
+	JButton remove = new JButton();
+	JButton save = new JButton();
+	JButton load = new JButton();
 	String file;
 	String killfile;
-	String Total;
 
 	public static void main(String[] args) {
 		ListTracker LT = new ListTracker();
@@ -34,36 +33,36 @@ public class ListTracker implements ActionListener {
 		f.add(p);
 		f.setVisible(true);
 		f.setSize(200, 130);
-		p.add(b);
-		p.add(b2);
-		p.add(b3);
-		p.add(b4);
-		b.setText("Add Task");
-		b2.setText("Remove Task");
-		b3.setText("Save");
-		b4.setText("Load");
-		b.addActionListener(this);
-		b2.addActionListener(this);
-		b3.addActionListener(this);
-		b4.addActionListener(this);
+		p.add(add);
+		p.add(remove);
+		p.add(save);
+		p.add(load);
+		add.setText("Add Task");
+		remove.setText("Remove Task");
+		save.setText("Save");
+		load.setText("Load");
+		add.addActionListener(this);
+		remove.addActionListener(this);
+		save.addActionListener(this);
+		load.addActionListener(this);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if (e.getSource().equals(b)) {
+		if (e.getSource().equals(add)) {
 			file = JOptionPane.showInputDialog("Add a task.");
 			Task.add(file);
 		}
-		if (e.getSource().equals(b2)) {
+		if (e.getSource().equals(remove)) {
 			killfile = JOptionPane.showInputDialog("Remove a task by giving a number.");
 			int y = Integer.parseInt(killfile);
 			Task.remove(y);
 		}
-		if (e.getSource().equals(b3)) {
+		if (e.getSource().equals(save)) {
+			String Total = "";
 			for (int i = 0; i < Task.size(); i++) {
-				Total += Task.get(i);
-
+				Total += Task.get(i) + "\n";
 			}
 			try {
 				FileWriter fw = new FileWriter("src/My_Program/test4.txt");
@@ -71,6 +70,32 @@ public class ListTracker implements ActionListener {
 				fw.close();
 			} catch (IOException e1) {
 				e1.printStackTrace();
+			}
+		}
+		if (e.getSource().equals(load)) {
+			String locationfile = JOptionPane.showInputDialog("Please input a file location.");
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(locationfile));
+				String line = br.readLine();
+				 Task = new ArrayList<String>();
+				while (line != null) {
+
+					Task.add(line);
+					line = br.readLine();
+				}
+				String iterator = "";
+				for (int i = 0; i < Task.size(); i++) {
+					iterator +=(Task.get(i)+ "\n");
+				}
+				
+				JOptionPane.showMessageDialog(null, iterator);
+				br.close();
+			} catch (FileNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (IOException e2) {
+				// TODO Auto-generated catch block
+				e2.printStackTrace();
 			}
 		}
 	}
